92906c990ac22b8cfa7a85ccda7f6600293caf12
louis baptiste <louisvis@ymail.com>
2020-03-31 01:00:37 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,11 @@
 // declare your functions here...
+function paragraphSelector(){
+  return $('p');
+}
+
+function ninjaBabySelector(){
+  return $('#baby-ninja');
+}
+function lastImageSelector(){
+  return $('li:last');
+}
